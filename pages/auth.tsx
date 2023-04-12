45b412ef3534031099daadac6e8d21c9dd9a3209
Sequajaa <sequaja.marco@gmail.com>
2023-04-13 01:35:20 +0200

    first commit

---
+++ pages/auth.tsx
@@ -0,0 +1,157 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { getSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import Input from "@/components/Input";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+const Auth = () => {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      });
+
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      });
+
+      login();
+    } catch (error) {
+        console.log(error);
+    }
+  }, [email, name, password, login]);
+
+  return (
+    <div className="relative h-full w-full bg-white">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)}
+                />
+              )}
+              <Input
+                id="email"
+                type="email"
+                label="Email address"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              onClick={variant === 'login' ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Sign in" : "Sign up"}
+            </button>
+            <div className="relative mt-7">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t border-slate-300" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-white px-2 text-slate-600">Or continue with</span>
+        </div>
+        </div>
+            <div className="flex flex-col items-center gap-4 mt-2 justify-center">
+              <button
+                onClick={() => signIn("google", { callbackUrl: "/api/auth/callback" })}
+                className="mt-7 flex items-center gap-2 bg-white text-gray-800 rounded-lg text-xl shadow-md py-2 px-8"
+              >
+                <FcGoogle size={32} />
+                <span className="text-xl">Sign in with Google</span>
+              </button>
+              <button
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="my-3 flex items-center gap-2 bg-white text-gray-800 rounded-lg text-xl shadow-md py-2 px-8"
+              >
+                <FaGithub size={32} />
+                <span className="text-xl">Sign in with GitHub</span>
+              </button>
+            </div>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "First time here?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
